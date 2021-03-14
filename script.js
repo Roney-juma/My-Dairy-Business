a08b6cd66d5b7c974015a4bd51d756978b490c5d
Roney-juma <ochirone10@gmail.com>
2021-03-15 01:49:31 +0300

    implimented the script to return output

---
+++ script.js
@@ -0,0 +1,6 @@
+function totalproduction(num1,num2,num3,num4){
+    
+    console.log(num1+num2+num3+num4)
+    var display = document.getElementById("display");
+    display.innerHTML = "The total production is" totalproduction()
+}
