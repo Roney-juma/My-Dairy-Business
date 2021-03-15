6e4e29c1544307c4907ebf4154b51089d6eae209
Roney-juma <ochirone10@gmail.com>
2021-03-15 11:23:24 +0300

    edited some parts of Js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-function totalproduction(num1,num2,num3,num4){
-    
-    console.log(num1+num2+num3+num4)
+function totalproduction(){
+    var shades=[];
+    shades.push(document.getElementById("num1").value);
+    shades.push(document.getElementById("num2").value);
+    shades.push(document.getElementById("num3").value);
+    shades.push(document.getElementById("num4").value);
     var display = document.getElementById("display");
-    display.innerHTML = "The total production is" totalproduction()
+    display.innerHTML = "The total production is" "num1"
 }
