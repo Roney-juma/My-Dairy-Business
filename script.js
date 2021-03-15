f6a3aa5a3d6e65313687912f65e02696c3938fd9
Roney-juma <ochirone10@gmail.com>
2021-03-15 15:43:10 +0300

    trying to make functions work out

--- script.js
+++ script.js
@@ -1,14 +1,52 @@
-function totalproduction(){
-    var shadesA=parseInt(document.getElementById("num1").value);
-    var shadesB=parseInt(document.getElementById("num2").value);
-    var shadesC=parseInt(document.getElementById("num3").value);
-    var shadesD=parseInt(document.getElementById("num4").value);
-    var litres= shadesA+shadesB+shadesC+shadesD;
-    document.getElementById("Litres").innerHTML=litres + "litres";
+function totalProduction(){
+
+    var shadeA = parseInt(document.getElementById("num1").value);
+    var shadeB = parseInt(document.getElementById("num2").value);
+    var shadeC = parseInt(document.getElementById("num3").value);
+    var shadeD = parseInt(document.getElementById("num4").value);
+        
+    var litres = shadeA + shadeB + shadeC + shadeD;
+    
+    document.getElementById("milkdisplay").innerHTML = litres + " Liters " ;
 
     document.getElementById("display-shade-a").innerHTML = "Shade A is "+ shadeA + " Liters " ;
     document.getElementById("display-shade-b").innerHTML = "Shade B is "+ shadeB + " Liters " ;
     document.getElementById("display-shade-c").innerHTML = "Shade C is "+ shadeC + " Liters " ;
     document.getElementById("display-shade-d").innerHTML = "Shade D is "+ shadeD + " Liters " ;
-}
 
+}
+window.onload = function() {
+    var addTotal = document.getElementById("farm-button");
+    addTotal.onclick = totalProduction;
+
+    var findIncome = document.getElementById("rate-update");
+    findIncome.onclick = incomeOverTime;
+
+function incomeOverTime(){
+
+    var shadeA = parseInt(document.getElementById("totalmilkA").value);
+    var shadeB = parseInt(document.getElementById("totalmilkB").value);
+    var shadeC = parseInt(document.getElementById("totalmilkC").value);
+    var shadeD = parseInt(document.getElementById("totalmilkD").value);
+    var rate = parseInt(document.getElementById("milkrate").value);
+
+    var totalMilk = shadeA + shadeB + shadeC + shadeD;
+
+
+    var day = 1;
+    var week = 7;
+    var month = 31;
+    var year = 365;
+
+        
+        var dailyIncome = day * rate * totalMilk;
+        var weeklyIncome = week * rate * totalMilk;
+        var monthlyIncome = month * rate * totalMilk;
+        var yearlyIncome = year * rate * totalMilk;
+
+    document.getElementById("display-2").innerHTML = dailyIncome + " KSH " ;
+    document.getElementById("display-3").innerHTML = monthlyIncome + " KSH " ;
+    document.getElementById("display-4").innerHTML = weeklyIncome + " KSH " ;
+    document.getElementById("display-5").innerHTML = yearlyIncome + " KSH " ;
+
+}
