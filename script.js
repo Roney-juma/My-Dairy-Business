af4a22522e2ea81f77b94557a05cdb1963bd24e1
Roney-juma <ochirone10@gmail.com>
2021-03-15 15:00:38 +0300

    edited some javascript

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function totalproduction(){
-    var shades=[];
-    shades.push(document.getElementById("num1").value);
-    shades.push(document.getElementById("num2").value);
-    shades.push(document.getElementById("num3").value);
-    shades.push(document.getElementById("num4").value);
-    var display = document.getElementById("display");
-    display.innerHTML = "The total production is" "num1"
+    var shadesA=parseInt(document.getElementById("num1").value);
+    var shadesB=parseInt(document.getElementById("num2").value);
+    var shadesC=parseInt(document.getElementById("num3").value);
+    var shadesD=parseInt(document.getElementById("num4").value);
+    var litres= shadesA+shadesB+shadesC+shadesD;
+    document.getElementById("Litres").innerHTML=litres + "litres";
+
+    document.getElementById("display-shade-a").innerHTML = "Shade A is "+ shadeA + " Liters " ;
+    document.getElementById("display-shade-b").innerHTML = "Shade B is "+ shadeB + " Liters " ;
+    document.getElementById("display-shade-c").innerHTML = "Shade C is "+ shadeC + " Liters " ;
+    document.getElementById("display-shade-d").innerHTML = "Shade D is "+ shadeD + " Liters " ;
 }
